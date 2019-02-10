bf8885952d3ea4ea9f45926795a0cea513bf9c2b
junhong cheon <cjswnsghd12@gmail.com>
2019-02-10 11:30:56 +0900

    라이브러리

---
+++ Shapes/Shapes/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    public class Circle
+    {
+        double radius;
+
+        public Circle(double radius = 0.0d)
+        {
+            this.radius = radius;
+        }
+        public double Area()
+        {
+            return Math.PI * (radius * radius);
+        }
+    }
+}
