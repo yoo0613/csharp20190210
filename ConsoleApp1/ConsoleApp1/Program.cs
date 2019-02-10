c61b4e939d4a9516caf8f7d818dee7c38e438936
junhong cheon <cjswnsghd12@gmail.com>
2019-02-10 09:59:31 +0900

    선택적 인수

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        private int m = 88, n = 99;
+        /*
+        Program()
+        {
+            this.m = 0;
+            this.n = 0;
+        }
+        */
+        //생성자
+        Program(int m = 0, int n = 0)
+        {
+            this.m = m;
+            this.n = n;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Sum(j:10,i:20));
+            Console.WriteLine(Sum(j: 10));
+            Console.WriteLine(Sum(20));
+            Console.WriteLine(Sum());
+
+            Program p1 = new Program();
+            Console.WriteLine($"m={p1.m}, n = {p1.n}");
+
+            Program p2 = new Program(1, 2);
+            Console.WriteLine($"m={p2.m}, n = {p2.n}");
+        }
+
+        static int Sum(int i=0, int j=0)
+        {
+            return i + j;
+        }
+    }
+}
