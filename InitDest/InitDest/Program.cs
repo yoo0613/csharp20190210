36d9216efaa99a8de75b18c0f972c83e743dd97e
junhong cheon <cjswnsghd12@gmail.com>
2019-02-10 15:15:37 +0900

    생성자

---
+++ InitDest/InitDest/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InitDest
+{
+    class MyBaseClass
+    {
+        public MyBaseClass()
+        {
+            Console.WriteLine(">>> MyBaseClass()");
+        }
+        public MyBaseClass(int i)
+        {
+            Console.WriteLine(">>> MyBaseClass(int i)");
+        }
+    }
+
+    class MyClass:MyBaseClass
+    {
+        public MyClass()
+        {
+            Console.WriteLine(">>> MyClass()");
+        }
+        public MyClass(int i):base(i)
+        {
+            Console.WriteLine(">>> MyClass(int i)");
+        }
+        public MyClass(int i, int j)
+        {
+            Console.WriteLine(">>> MyClass(int i, int j)");
+        }
+        public MyClass(int i, int j, int k) : base(i)
+        {
+            Console.WriteLine(">>> MyClass(int i, int j, int k) ");
+        }
+        public MyClass(int i, int j, int k, int l) : this(i,j)
+        {
+            Console.WriteLine(">>> MyClass(int i, int j, int k, int l) ");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass m1 = new MyClass();
+            MyClass m2 = new MyClass(1);
+            MyClass m3 = new MyClass(1,2);
+            MyClass m4 = new MyClass(1,2,3);
+            MyClass m5 = new MyClass(1,2,3,4);
+        }
+    }
+}
