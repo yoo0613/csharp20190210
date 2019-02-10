df48d70eaadb0ec8119afad36329dbc5875674ed
junhong cheon <cjswnsghd12@gmail.com>
2019-02-10 17:50:55 +0900

    가변길이 배열, 2차원배열

---
+++ ArrayLab/ArrayLab/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ArrayLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] a = { new int[] { 1, 2 }, new int[] { 1, 2, 3 }, new int[] { 1, 2, 3, 4 } };
+
+            int[,] b = { { 1, 2 }, { 3, 4 }, { 5, 6 } };
+
+            foreach (int[] i in a)
+            {
+                foreach (int j in i)
+                {
+                    Console.Write(j + " ");
+                }
+            }
+
+            Console.WriteLine("\n--------------------\n");
+
+            foreach (int i in b)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
