bac0ba1b30c4423c0ec2f47a904fe29dba927ad6
junhong cheon <cjswnsghd12@gmail.com>
2019-02-10 09:44:05 +0900

    가변길이 함수

---
+++ Params/Params/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Params
+{
+    class Program
+    {
+        static void OnjSum(string title, params int[] num)
+        {
+            int sum = 0;
+            Console.WriteLine("Sum of {0}", title);
+
+            foreach (int i in num)
+                sum += i;
+
+            Console.WriteLine("Sum :: {0}\n", sum);
+        }
+
+        static void Main(string[] args)
+        {
+            int[] onjArray = new int[2] { 1, 2 };
+
+            OnjSum("숫자하나", 1);
+            OnjSum("숫자둘", 1, 2);
+            OnjSum("숫자셋", 1, 2, 3);
+            OnjSum("배열", onjArray);
+        }
+    }
+}
